fb1fb88d9c07baeacb9a1cd9c36a7810ea9254a3
ANDRES GARCES <ANDRES GARCES@192.168.10.17>
2024-03-10 22:02:15 -0500

    Segundo Commit

--- src/MétodosListas/NombreDeLaClase.java
+++ src/MétodosListas/NombreDeLaClase.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Métodos;
+package MétodosListas;
 
 /**
  *
@@ -56,6 +56,5 @@ public class NombreDeLaClase {
 
     public void setCódigo(int Código) {
         this.Código = Código;
-    }
-    
+    }   
 }
